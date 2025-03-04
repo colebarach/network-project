53ae3aeeb68e784fd960f73b3a9fc0fab9083742
Cole Barach <colebarach@tutanota.com>
2025-03-04 11:20:52 -0500

    Setup clangd for the adapter firmware.

--- src/network_layer/src/main.c
+++ src/network_layer/src/main.c
@@ -3,19 +3,26 @@
 // RPI Pico SDK
 #include "pico/stdlib.h"
 
-// Entrypoint -----------------------------------------------------------------------------------------------------------------
+// Core 0 Entrypoint ----------------------------------------------------------------------------------------------------------
 
 int main ()
 {
-    const uint16_t LED_PIN = 0;
-    gpio_init (LED_PIN);
-    gpio_set_dir (LED_PIN, GPIO_OUT);
+	const uint16_t LED_PIN = 0;
+	gpio_init (LED_PIN);
+	gpio_set_dir (LED_PIN, GPIO_OUT);
 
-    while (true)
+	while (true)
 	{
-        sio_hw->gpio_out = 0x01;
-        sleep_ms (250);
-        sio_hw->gpio_out = 0x00;
-        sleep_ms (250);
-    }
+		sio_hw->gpio_out = 0x01;
+		sleep_ms (250);
+		sio_hw->gpio_out = 0x00;
+		sleep_ms (250);
+	}
+}
+
+// Core 1 Entrypoint ----------------------------------------------------------------------------------------------------------
+
+int main1 ()
+{
+	while (true) {}
 }
