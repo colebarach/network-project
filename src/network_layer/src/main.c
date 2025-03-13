02c45a618cb5a87e8e4991855b69d50df1e77ecf
Cole Barach <colebarach@tutanota.com>
2025-03-13 11:11:24 -0400

    Network adapters are now able to detect collisions. At the minute, I have 2 nodes transmitting unique datagrams at random, and 1 node listening for traffic. Upon a collision, the two offending nodes both transmit a jamming frame with the listening node can detect.

--- src/network_layer/src/main.c
+++ src/network_layer/src/main.c
@@ -1,23 +1,146 @@
+// TODO(Barach): This is using an old version of the Pico SDK, should update to a stable release.
+
 // Includes -------------------------------------------------------------------------------------------------------------------
 
 // RPI Pico SDK
 #include "pico/stdlib.h"
 
+// C Standard Library
+#include <stdlib.h>
+#include <string.h>
+
+// Constants ------------------------------------------------------------------------------------------------------------------
+
+#define ADDR_A 1
+#define TX_MODE 0
+
+#define BAUDRATE		115200
+#define UART0_TX		0
+#define UART0_RX		1
+#define UART0_RX_SENSE	2
+#define COLLISION_PIN	3
+
+void jamSequence ()
+{
+	gpio_set_function (UART0_TX, GPIO_FUNC_SIO);
+	gpio_set_dir (UART0_TX, GPIO_OUT);
+
+	bool pin = true;
+	for (uint8_t i = 0; i < 8; ++i)
+	{
+		gpio_put (UART0_TX, pin);
+		pin = !pin;
+		sleep_us (4);
+	}
+
+	sleep_us (400);
+
+	gpio_set_function (UART0_TX, GPIO_FUNC_UART);
+}
+
 // Core 0 Entrypoint ----------------------------------------------------------------------------------------------------------
 
 int main ()
 {
-	const uint16_t LED_PIN = 0;
-	gpio_init (LED_PIN);
-	gpio_set_dir (LED_PIN, GPIO_OUT);
+	uart_init (uart0, BAUDRATE);
+
+	gpio_set_function (UART0_TX, GPIO_FUNC_UART);
+	gpio_set_function (UART0_RX, GPIO_FUNC_UART);
+
+	gpio_init (UART0_RX_SENSE);
+	gpio_set_dir (UART0_RX_SENSE, GPIO_IN);
+	gpio_init (COLLISION_PIN);
+	gpio_set_dir (COLLISION_PIN, GPIO_OUT);
+
+	#if ADDR_A
+	uint8_t txBuffer [4] = { 0xAB, 0xCD, 0x12, 0x34 };
+	#else
+	uint8_t txBuffer [4] = { 0x87, 0x65, 0xDC, 0xBA };
+	#endif
+
+	uint8_t rxBuffer;
+
+	#if TX_MODE
 
 	while (true)
 	{
-		sio_hw->gpio_out = 0x01;
-		sleep_us (50);
-		sio_hw->gpio_out = 0x00;
-		sleep_us (50);
+		// Random CSMA delay, min of 400 us
+		uint16_t target = (rand () % 512) + 400;
+
+		// Block until the bus is idle for the target interval.
+		uint16_t count = 0;
+		while (true)
+		{
+			// If not idle, reset timer
+			if (!gpio_get (UART0_RX_SENSE))
+				count = 0;
+			else
+				++count;
+
+			if (count > target)
+				break;
+
+			sleep_us (1);
+		}
+
+		gpio_put (COLLISION_PIN, false);
+
+		// Transmit byte by byte, checking for collision.
+		for (uint8_t index = 0; index < sizeof (txBuffer); ++index)
+		{
+			// Empty the UART's RX FIFO
+			while (uart_is_readable(uart0))
+				uart_read_blocking (uart0, &rxBuffer, sizeof (rxBuffer));
+
+			// Transmit the byte
+			uart_write_blocking (uart0, txBuffer + index, 1);
+
+			sleep_us (96);
+
+			// No byte read => collision
+			if (!uart_is_readable (uart0))
+			{
+				gpio_put (COLLISION_PIN, true);
+				jamSequence ();
+				break;
+			}
+
+			// Read the byte that was transmitted
+			uart_read_blocking (uart0, &rxBuffer, sizeof (rxBuffer));
+
+			// Byte mismatch => collision
+			if (txBuffer [index] != rxBuffer)
+			{
+				gpio_put (COLLISION_PIN, true);
+				jamSequence ();
+				break;
+			}
+		}
 	}
+
+	#else
+
+	while (true)
+	{
+		// Read the byte that was transmitted
+		while (!uart_is_readable (uart0))
+			tight_loop_contents ();
+
+		gpio_put (COLLISION_PIN, false);
+
+		volatile uint32_t d = uart_get_hw (uart0)->dr;
+		if (d > 0xFF)
+		{
+			gpio_put (COLLISION_PIN, true);
+
+			sleep_us (100);
+
+			while (uart_is_readable (uart0))
+				uart_read_blocking (uart0, &rxBuffer, sizeof (rxBuffer));
+		}
+	}
+
+	#endif
 }
 
 // Core 1 Entrypoint ----------------------------------------------------------------------------------------------------------
