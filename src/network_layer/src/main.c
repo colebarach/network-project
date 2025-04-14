dc3c0cd37e4cd0214b501aa7b6988123fd8f8ac8
Cole Barach <colebarach@tutanota.com>
2025-04-14 13:32:59 -0400

    General refactor

--- src/network_layer/src/main.c
+++ src/network_layer/src/main.c
@@ -11,25 +11,28 @@
 
 // Constants ------------------------------------------------------------------------------------------------------------------
 
-#define SEED 0
-#define TX_MODE 1
+#define SEED				1
+#define TX_MODE				0
 
-#define BAUDRATE		115200
-#define UART0_TX		0
-#define UART0_RX		1
-#define UART0_RX_SENSE	2
-#define COLLISION_PIN	3
+#define BAUDRATE			115200
 
-void jamSequence ()
-{
-	gpio_set_function (UART0_TX, GPIO_FUNC_SIO);
-	gpio_set_dir (UART0_TX, GPIO_OUT);
+#define FRAME_TIME_US		(11 * 1000000 / BAUDRATE + 1)
+#define INTERFRAME_TIME_US	(FRAME_TIME_US / 8)
 
-	gpio_put (UART0_TX, false);
-	sleep_us (120);
+#define UART0_TX			0
+#define UART0_RX			1
+#define UART0_RX_SENSE		2
+#define COLLISION_PIN		3
 
-	gpio_set_function (UART0_TX, GPIO_FUNC_UART);
-}
+// Functions Prototypes -------------------------------------------------------------------------------------------------------
+
+void waitForIdle ();
+
+void transmitJamFrame ();
+
+bool transmitDatagram (uint8_t* txBuffer, uint16_t txSize);
+
+bool receiveDatagram (uint8_t* rxBuffer, uint16_t rxSize);
 
 // Core 0 Entrypoint ----------------------------------------------------------------------------------------------------------
 
@@ -47,78 +50,18 @@ int main ()
 
 	uint8_t txBuffer [4];
 
-	uint8_t rxBuffer;
-
 	#if TX_MODE
 
 	srand (SEED);
 
 	while (true)
 	{
-		// Random payload
-		for (uint8_t index = 0; index < sizeof (txBuffer); ++index)
-			txBuffer [index] = rand ();
-
-		// Random CSMA delay, min of 400 us
-		uint16_t target = (rand () % 512) + 120;
-
-		// Block until the bus is idle for the target interval.
-		uint16_t count = 0;
-		while (true)
+		// Transmit the next datagram.
+		if (transmitDatagram (txBuffer, sizeof (txBuffer)))
 		{
-			// If not idle, reset timer
-			if (!gpio_get (UART0_RX_SENSE))
-				count = 0;
-			else
-				++count;
-
-			if (count > target)
-				break;
-
-			sleep_us (1);
-		}
-
-		gpio_put (COLLISION_PIN, false);
-
-		// Transmit byte by byte, checking for collision.
-		for (uint8_t index = 0; index < sizeof (txBuffer); ++index)
-		{
-			uart_get_hw (uart0)->rsr = 0x00;
-
-			// Empty the UART's RX FIFO
-			while (uart_is_readable(uart0))
-				uart_read_blocking (uart0, &rxBuffer, sizeof (rxBuffer));
-
-			// Transmit the byte
-			uart_write_blocking (uart0, txBuffer + index, 1);
-
-			sleep_us (100);
-
-			// No byte read => collision
-			if (!uart_is_readable (uart0))
-			{
-				gpio_put (COLLISION_PIN, true);
-				jamSequence ();
-				break;
-			}
-
-			// Read the byte that was transmitted. UART error => collision
-			uint32_t dr = uart_get_hw (uart0)->dr;
-			rxBuffer = (uint8_t) dr;
-			if ((dr & 0xFF00) != 0x0000 && (dr & 0xFF00) != 0x0800)
-			{
-				gpio_put (COLLISION_PIN, true);
-				jamSequence ();
-				break;
-			}
-
-			// Byte mismatch => collision
-			if (txBuffer [index] != rxBuffer)
-			{
-				gpio_put (COLLISION_PIN, true);
-				jamSequence ();
-				break;
-			}
+			// If successful, generate a new random payload.
+			for (uint8_t index = 0; index < sizeof (txBuffer); ++index)
+				txBuffer [index] = rand ();
 		}
 	}
 
@@ -126,22 +69,10 @@ int main ()
 
 	while (true)
 	{
-		// Read the byte that was transmitted
-		while (!uart_is_readable (uart0))
-			tight_loop_contents ();
-
-		gpio_put (COLLISION_PIN, false);
-
-		uint32_t dr = uart_get_hw (uart0)->dr;
-		if (dr > 0xFF)
-		{
-			gpio_put (COLLISION_PIN, true);
-
-			sleep_us (100);
-
-			while (uart_is_readable (uart0))
-				uart_read_blocking (uart0, &rxBuffer, sizeof (rxBuffer));
-		}
+		volatile bool test = receiveDatagram (txBuffer, sizeof (txBuffer));
+		test = test;
+		test = transmitDatagram (txBuffer, sizeof (txBuffer));
+		test = test;
 	}
 
 	#endif
@@ -152,4 +83,145 @@ int main ()
 int main1 ()
 {
 	while (true) {}
+}
+
+// Functions ------------------------------------------------------------------------------------------------------------------
+
+void waitForIdle ()
+{
+	// Random CSMA delay, min of 2 frames, max of 10 frames
+	uint16_t delayUs = (rand () % (8 * FRAME_TIME_US)) + 2 * FRAME_TIME_US;
+
+	// Block until the bus is idle for the target interval.
+	uint16_t count = 0;
+	while (true)
+	{
+		// If not idle, reset timer
+		if (!gpio_get (UART0_RX_SENSE))
+			count = 0;
+		else
+			++count;
+
+		if (count >= delayUs)
+			break;
+
+		sleep_us (1);
+	}
+}
+
+void flushReceiveFifo ()
+{
+	uint8_t buffer;
+	while (uart_is_readable(uart0))
+		uart_read_blocking (uart0, &buffer, 1);
+}
+
+void transmitJamFrame ()
+{
+	// Debugging output high
+	gpio_put (COLLISION_PIN, true);
+
+	// Set the TX pin to a GPIO output.
+	gpio_set_function (UART0_TX, GPIO_FUNC_SIO);
+	gpio_set_dir (UART0_TX, GPIO_OUT);
+
+	// Hold the bus in the dominant state for 2 frames. This guarantees all devices receive a break error and notice the
+	// collision occurred.
+	gpio_put (UART0_TX, false);
+	sleep_us (2 * FRAME_TIME_US);
+
+	// Set the TX pin back to a UART pin.
+	gpio_set_function (UART0_TX, GPIO_FUNC_UART);
+
+	// Debugging output low
+	gpio_put (COLLISION_PIN, false);
+}
+
+bool transmitDatagram (uint8_t* txBuffer, uint16_t txSize)
+{
+	// Wait until the bus is idle
+	waitForIdle ();
+
+	// Transmit byte by byte, checking for collision.
+	for (uint8_t index = 0; index < txSize; ++index)
+	{
+		uart_get_hw (uart0)->rsr = 0x00;
+
+		// Empty the UART's RX FIFO
+		flushReceiveFifo ();
+
+		// Transmit the byte, block until completion
+		uart_write_blocking (uart0, txBuffer + index, 1);
+		sleep_us (FRAME_TIME_US);
+
+		// No byte read => collision
+		if (!uart_is_readable (uart0))
+		{
+			transmitJamFrame ();
+			return false;
+		}
+
+		// Read the byte that was transmitted. If break error, parity error, or framing error then a collision occurred.
+		uint32_t dr = uart_get_hw (uart0)->dr;
+		if ((dr & 0xFF00) != 0x0000 && (dr & 0xFF00) != 0x0800)
+		{
+			transmitJamFrame ();
+			return false;
+		}
+
+		// Byte mismatch => collision
+		if (txBuffer [index] != (uint8_t) dr)
+		{
+			transmitJamFrame ();
+			return false;
+		}
+	}
+
+	// Success
+	return true;
+}
+
+bool receiveDatagram (uint8_t* rxBuffer, uint16_t rxSize)
+{
+	// Debugging pin
+	gpio_put (COLLISION_PIN, false);
+
+	// Block until the first new byte is received.
+	flushReceiveFifo ();
+	while (!uart_is_readable (uart0));
+
+	uint16_t index = 0;
+	while (true)
+	{
+		// If the datagram exceeds the buffer size, fail.
+		if (index >= rxSize)
+			return false;
+
+		// If any UART errors occurred, a collision occurred.
+		uint32_t dr = uart_get_hw (uart0)->dr;
+		if (dr > 0xFF)
+		{
+			gpio_put (COLLISION_PIN, true);
+			return false;
+		}
+
+		// Read the byte into the buffer
+		rxBuffer [index] = dr;
+		++index;
+
+		// Wait for the next byte, or timeout
+		uint16_t count = 0;
+		while (true)
+		{
+			if (uart_is_readable (uart0))
+				break;
+
+			if (count > INTERFRAME_TIME_US)
+				return true;
+
+			++count;
+
+			sleep_us (1);
+		}
+	}
 }
