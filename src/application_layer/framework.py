ebcc71780d311cae0ab42bc9abec8c548b62926c
Jacob Koshel <jacob@localhost.localdomain>
2025-04-25 17:51:48 -0400

    Kinda working stdin and stdout

--- src/application_layer/framework.py
+++ src/application_layer/framework.py
@@ -1,15 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import subprocess, sys
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    title = 'Welcome to Our Website!'
-    return render_template('home.html', title=title)
+    # Check if there's piped input (data from stdin)
+    piped_data = None
+    if not sys.stdin.isatty():
+        piped_data = sys.stdin.read()  # Capture piped input
+        # You could process or store the data here if needed
+
+    return render_template('home.html', piped_data=piped_data)
+
 
 @app.route('/main/')
 def about():
     return render_template('main.html')
+
+@app.route('/send/', methods=['GET', 'POST'])
+def send():
+    if request.method == 'POST':
+        message = request.form['message']
+        
+        # Make a system call to echo the message in the terminal
+        if message:
+            # Use subprocess to run the echo command
+            subprocess.run(['echo', message])  # This will print the message in the terminal
+        
+    return render_template('send.html')
+
+
+@app.route('/receive/')
+def receive():
+    terminal_data = "No input provided"  # Default message
+    if not sys.stdin.isatty():
+        # Read data from stdin if it was piped
+        terminal_data = sys.stdin.read().strip()
+        
+    return render_template('receive.html', terminal_data=terminal_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
